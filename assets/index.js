e970c47576b32f36c96dec30820d03affe4e1321
Julie Suzuya <juliesuzuya@outlook.com>
2023-11-23 19:14:13 -0300

    .

---
+++ assets/index.js
@@ -0,0 +1,42 @@
+const overlay = document.getElementById('overlay');
+const query = new URLSearchParams(window.location.search);
+
+class SoloBoom {
+
+  static #endpoint = "https://soloboomchallenge.com/backend/api/Stats?tournamentId=fa942c88-07d6-41b7-87a2-b4398fa725d9";
+
+  static async #sbc() {
+    const response = await fetch(this.#endpoint);
+    const data = await response.json();
+    return data;
+  }
+
+  static #player_data(player) {
+    player.rankStatus = player.rankStatus === ' | 0 LP' ? "MD5" : player.rankStatus;
+    return player;
+  }
+
+  static async search_player(streamerName) {
+    const data = await this.#sbc();
+    const player = data.find(i => i.streamerName.toLowerCase() === streamerName.toLowerCase());
+
+    if (!player) throw new Error("no_player");
+
+    return this.#player_data(player);
+  }
+
+}
+
+async function run() {
+  try {
+    if (!query.get('streamerName')) return;
+    const { rankStatus } = await SoloBoom.search_player(query.get('streamerName'));
+    overlay.innerHTML = `Elo: ${rankStatus}`;
+  } catch (e) {
+    if (e.message == "no_player") return overlay.innerHTML = `${query.get('streamerName')} não está participando do SoloBoom`;
+    return overlay.innerHTML = e.message;
+  };
+};
+
+run();
+setInterval(run, 60000);
